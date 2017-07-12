4e3e39ede724cae33f37258dd4396bc8290795d5
zhufengnodejs <zhufengnodejs@126.com>
2017-07-12 12:07:54 +0800

    1.加载首页数据

--- public/javascripts/vote.js
+++ public/javascripts/vote.js
@@ -30,20 +30,38 @@ let voteFn = {
                         </div>     
                     </li>`
     )
+  },
+  request({url,type='GET',dataType='json',data={},success}){
+    $.ajax({url, type, dataType, data, success});
+  },
+  loadUsers(){
+    voteFn.request({
+      url:'/vote/index/data',
+      data:{limit,offset},
+      success(result){
+        let users = result.data.objects;
+        //修改偏移量1 偏移量0 第二页 10
+        offset+=users.length;
+        //TODO 把users数组转成li数组并且添加到ul里
+        let html = users.map(user=>voteFn.formatUser(user)).join('');
+        $('.coming').append(html);
+      }
+    });
   }
 }
 $(function(){
-  $.ajax({
-    url:'/vote/index/data',
-    type:'GET',
-    dataType:'json',
-    data:{limit,offset},
-    success(result){
-      console.log(result);
-      let users = result.data.objects;
-      //TODO 把users数组转成li数组并且添加到ul里
-      let html = users.map(user=>voteFn.formatUser(user)).join('');
-      $('.coming').html(html);
+  //1.少传几个变量，不再可能漏写，也不可能出错
+  //2.如果以后不用jquery, 只改一个地方就可以了 jquery axios fetch
+  voteFn.loadUsers();
+  window.onscroll= function(){
+    //屏幕的高度
+    let clientHeight = document.documentElement.clientHeight||document.body.clientHeight;
+    //向上卷去的高度
+    let scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+    //内容的高度
+    let scrollHeight =  document.documentElement.scrollHeight||document.body.scrollHeight;
+    if(clientHeight+scrollTop+20>=scrollHeight){
+      voteFn.loadUsers();
     }
-  });
+  }
 });
